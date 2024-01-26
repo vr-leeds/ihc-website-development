f64166b9fd065c3eb99bb4b52ab1c458eb162fb6
George Gabriel <g.andrew.gabriel@gmail.com>
2024-01-26 11:53:31 +0000

    Complete restyle

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,17 +20,21 @@ module.exports = {
         "colour-text-main": "#353535",
 
         "c-bg-l": "#eff1f8",
+        "c-bg2-l": "#f4f5f7",
         "c-txt-l": "#393939",
         "c-fg1-l": "#ffffff",
         "c-fg2-l": "#a4b6c2",
         "c-fg3-l": "#ec669f",
+        "c-fg4-l": "#26C6DA",
         "c-h-l": "#582dd6",        
 
         "c-bg-d": "#ffffff",
+        "c-bg2-d": "#f4f5f7",
         "c-txt-d": "#393939",
         "c-fg1-d": "#a4b6c2",
         "c-fg2-d": "#d0d0c4",
         "c-fg3-d": "#ec669f",
+        "c-fg4-d": "#26C6DA",
         "c-h-d": "#582dd6"
 
 
